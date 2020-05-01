aa4a53f3d865e8e7d161c99748223945ff16739e
Tim-Niklas Reck <stu114743@mail.uni-kiel.de>
2020-05-01 17:35:40 +0200

    updated aop and monitoring script

--- run-monitoring.bat
+++ run-monitoring.bat
@@ -11,12 +11,13 @@ SET BINDIR=%cd%
 
 SET KIEKER_JAR="kieker-1.14-SNAPSHOT-aspectj.jar"
 SET KIEKER_CONFIG=".\META-INF\kieker.monitoring.sa.properties" 
-SET AOP_CONFIG=".\META-INF\aop.springPetclinic.xml"
+REM SET AOP_CONFIG=".\META-INF\aop.springPetclinic.xml"
+SET AOP_CONFIG=".\META-INF\aop.sa.xml"
 SET MAINCLASS="org.springframework.samples.petclinic.PetClinicApplication"
 SET JAVA_CP=".;BOOT-INF\classes\;BOOT-INF\lib\*"
 
 START "springPetclinic" /B java -javaagent:%KIEKER_JAR% -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Dkieker.monitoring.configuration=%KIEKER_CONFIG% -Dorg.aspectj.weaver.loadtime.configuration=%AOP_CONFIG% -cp %JAVA_CP% %MAINCLASS%
 
-CALL .\create-load.bat
+REM CALL .\create-load.bat
 
 REM java -javaagent:"kieker-1.14-SNAPSHOT-aspectj.jar" -Dkieker.monitoring.skipDefaultAOPConfiguration=true -Dkieker.monitoring.configuration=".\META-INF\kieker.monitoring.properties" -Dorg.aspectj.weaver.loadtime.configuration=".\META-INF\aop.springPetclinic.xml" -cp ".:BOOT-INF/classes/:BOOT-INF/lib/*" "org.springframework.samples.petclinic.PetClinicApplication"
